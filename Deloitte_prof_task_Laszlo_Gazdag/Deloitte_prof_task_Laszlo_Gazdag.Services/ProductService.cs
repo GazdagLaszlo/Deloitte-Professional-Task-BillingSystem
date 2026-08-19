f6b1258e1ff7657642458138fbb9e9b185eee262
GazdagLaszlo <glaci0206@gmail.com>
2026-08-19 13:04:17 +0200

    Implement Product endpoints

---
+++ Deloitte_prof_task_Laszlo_Gazdag/Deloitte_prof_task_Laszlo_Gazdag.Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.Context;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.DTOs;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deloitte_prof_task_Laszlo_Gazdag.Services
+{
+    public interface IProductService
+    {
+        Task<ProductDTO> CreateProductAsync(ProductCreateDTO productCreateDto);
+    }
+    public class ProductService : IProductService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public ProductService(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDTO> CreateProductAsync(ProductCreateDTO productCreateDto)
+        {
+            var product = _mapper.Map<Product>(productCreateDto);
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductDTO>(product);
+        }
+    }
+}
---
+++ Deloitte_prof_task_Laszlo_Gazdag/Deloitte_prof_task_Laszlo_Gazdag/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Deloitte_prof_task_Laszlo_Gazdag.DataContext.DTOs;
+using Deloitte_prof_task_Laszlo_Gazdag.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deloitte_prof_task_Laszlo_Gazdag.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ProductController(IProductService productService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] ProductCreateDTO createDto)
+        {
+            var result = await productService.CreateProductAsync(createDto);
+            return Ok(result);
+        }
+    }
+}
