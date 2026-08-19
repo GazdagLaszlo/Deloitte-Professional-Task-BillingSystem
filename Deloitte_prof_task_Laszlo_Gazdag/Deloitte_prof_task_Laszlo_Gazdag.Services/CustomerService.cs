cf2f67cbb69481e8137874f8d7612e5ccc6b4138
GazdagLaszlo <glaci0206@gmail.com>
2026-08-19 13:04:02 +0200

    Implement Customer endpoints

---
+++ Deloitte_prof_task_Laszlo_Gazdag/Deloitte_prof_task_Laszlo_Gazdag.Services/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.Context;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.DTOs;
+using Deloitte_prof_task_Laszlo_Gazdag.DataContext.Entities;
+
+namespace Deloitte_prof_task_Laszlo_Gazdag.Services
+{
+    public interface ICustomerService
+    {
+        Task<CustomerDTO> CreateCustomerAsync(CustomerCreateDTO customerCreateDto);
+    }
+
+    public class CustomerService : ICustomerService
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CustomerService(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomerDTO> CreateCustomerAsync(CustomerCreateDTO customerCreateDto)
+        {
+            var customer = _mapper.Map<Customer>(customerCreateDto);
+
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<CustomerDTO>(customer);
+        }
+    }
+}
---
+++ Deloitte_prof_task_Laszlo_Gazdag/Deloitte_prof_task_Laszlo_Gazdag/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Deloitte_prof_task_Laszlo_Gazdag.DataContext.DTOs;
+using Deloitte_prof_task_Laszlo_Gazdag.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deloitte_prof_task_Laszlo_Gazdag.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CustomerController(ICustomerService customerService) : ControllerBase
+    {         
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] CustomerCreateDTO createDto)
+        {
+            var result = await customerService.CreateCustomerAsync(createDto);
+            return Ok(result);
+        }
+    }
+}
