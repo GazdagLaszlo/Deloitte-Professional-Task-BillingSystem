66e8efe91a6ee5aaf7a2aa1c554731075076155f
GazdagLaszlo <glaci0206@gmail.com>
2026-08-19 09:52:46 +0200

    Project setup

---
+++ Deloitte_prof_task_Laszlo_Gazdag/Deloitte_prof_task_Laszlo_Gazdag.DataContext/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deloitte_prof_task_Laszlo_Gazdag.DataContext.Context
+{
+    internal class AppDbContext
+    {
+    }
+}
